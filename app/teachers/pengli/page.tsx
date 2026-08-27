4aa026e25a6174de5b1ba2934f33f6cced215c53
iamflashon <iamflashon@gmail.com>
2026-08-27 09:28:52 +0800

    Remove duplicate Pengli brand home link

--- app/teachers/pengli/page.tsx
+++ app/teachers/pengli/page.tsx
@@ -45,7 +45,6 @@ const samplePoints = [
 export default function PengliTeacherPage() {
   return <main className="pengli-page">
     <nav className="pengli-topbar" aria-label="頁面導覽">
-      <Link href="/" className="pengli-brand">iBrain Pedia X</Link>
       <div><span>法律類</span><b>行政法</b></div>
     </nav>
 
